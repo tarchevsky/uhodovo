851b49becf7eb389e6cca22958e3fac1597b805b
tarchevsky <i.tarchevsky@gmai.com>
2024-07-26 12:55:00 +0300

    Ещё одна версия metrika + обновление yarn lock

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,32 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Metrika } from '@/components/metrika/Metrika'
-import { Suspense } from 'react'
 
 export default function Document() {
 	return (
 		<Html lang='ru'>
-			<Head />
+			<Head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(97923828, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+                });
+              `
+					}}
+				/>
+			</Head>
+
 			<body>
 				<Main />
 				<NextScript />
-				<Suspense>
-					<Metrika />
-				</Suspense>
 			</body>
 		</Html>
 	)
