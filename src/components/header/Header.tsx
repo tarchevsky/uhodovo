1b058f5edd103a6c596a63923f02ab8acea92894
tarchevskyivan <i.tarchevsky@gmail.com>
2025-04-11 18:56:36 +0300

    wrap ссылок в header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 			<nav className='w-full h-full'>
 				<ul
 					tabIndex={0}
-					className='menu p-0 xl:p-6 flex-nowrap gap-2 xl:gap-5 menu-horizontal font-bold text-base-100 uppercase text-[10px] md:text-sm'
+					className='menu p-0 xl:p-6 flex-wrap gap-2 xl:gap-5 menu-horizontal font-bold text-base-100 uppercase text-[10px] md:text-sm'
 				>
 					<li className={cn(styles.item, 'block text-center')}>
 						<Link className='px-0 xl:px-[10px] btn btn-ghost' href='/#take'>
